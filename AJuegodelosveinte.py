bd14b5354b88a0a144a65f785f39b0833255668a
jc <jcbb1995@hotmail.com>
2020-06-16 17:07:26 -0500

    1er commit: Se agrego el archivo AJuegodelosveinte.py

---
+++ AJuegodelosveinte.py
@@ -0,0 +1,18 @@
+"""
+Escriba un programa que muestre números al azar del 1 al 6
+mientras lo pida el usuario.
+"""
+from random import randrange
+suma = 0
+
+print("Los 20 (1)")
+
+entrada = input("Pulse intro para lanzar el Dado. Pulse otra tecla e Intro para Terminar: ")
+
+while entrada == "":
+    dado = randrange(1,7)
+    print(f"Tirada: {dado}", f"Suma: {suma}")
+    entrada = input("Pulse intro para lanzar el Dado. Pulse otra tecla e Intro para Terminar: ")
+
+print("Programa terminado")
+
