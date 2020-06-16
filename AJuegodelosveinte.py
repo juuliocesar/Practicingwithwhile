0d946e4e68ccdaa4212eeaccdc1c3249a9c15e5b
jc <jcbb1995@hotmail.com>
2020-06-16 17:16:01 -0500

    Se agrego un segundo jugador

--- AJuegodelosveinte.py
+++ AJuegodelosveinte.py
@@ -1,19 +1,31 @@
-"""
-Escriba un programa que muestre números al azar del 1 al 6
-mientras lo pida el usuario.
-"""
+
 from random import randrange
-suma = 0
+suma1 = 0
+suma2 = 0
 
 print("Los 20 (1)")
 
 entrada = input("Pulse intro para lanzar el Dado. Pulse otra tecla e Intro para Terminar: ")
 
 while entrada == "":
-    dado = randrange(1,7)
-    suma = suma + dado
-    print(f"Tirada: {dado}", f"Suma: {suma}")
+    juj1 = randrange(1,7)
+    juj2 = randrange(1,7)
+    suma1 = suma1 + juj1
+    suma2 = suma2 + juj2
+    
+    print("Jugador 1 -",f"Tirada: {juj1}", f"Suma: {suma1}")
+    print("Jugador 2 -",f"Tirada: {juj2}", f"Suma: {suma2}")
+
     entrada = input("Pulse intro para lanzar el Dado. Pulse otra tecla e Intro para Terminar: ")
 
-print("Programa terminado")
+if entrada != "" and suma1==0 and suma2==0:
+    print("No se ha lanzado ningún dado")
 
+elif suma1>suma2:
+    print("Ha ganado el jugador 1")
+
+elif suma1<suma2:
+    print("Ha ganado el jugador 2")
+    
+else:
+    print("¡EMPATE!")
